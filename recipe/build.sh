710717849b3ee5f9698155a307f3a311130757f8
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-03-04 22:11:27 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,16 @@ set -xe
 mkdir -vp ${PREFIX}/bin
 
 # Set the assembler to `nasm`
-if [[ ${target_platform} == "linux-64" || ${target_platform} == "osx-64" ]]; then
+if [[ ${target_platform} == "linux-64" || ${target_platform} == "osx-64" || ${target_platform} == "linux-aarch64" ]]; then
     export AS="${BUILD_PREFIX}/bin/nasm"
 fi
 
-if [ ${target_platform} == "linux-aarch64" ]; then
-    export CFLAGS="-mpreferred-stack-boundary=8 ${CFLAGS}"
-    export CXXFLAGS="-mpreferred-stack-boundary=8 ${CXXFLAGS}"
-fi
+
+
+# if [ ${target_platform} == "linux-aarch64" ]; then
+#     export CFLAGS="-mpreferred-stack-boundary=8 ${CFLAGS}"
+#     export CXXFLAGS="-mpreferred-stack-boundary=8 ${CXXFLAGS}"
+# fi
 
 # EXTRA_CFLAGS="-Wall -g -m64 -pipe -O2 -fPIC"
 # if [[ $ARCH = 64 ]]; then
