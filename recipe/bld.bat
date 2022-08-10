abe2c2621db72e0ced2f7a9b266e2968e5304295
Mark Harfouche <mark.harfouche@gmail.com>
2022-08-10 14:40:46 -0400

    explicitely disable the static libraries on windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,11 @@
 rem Set the assembler to `nasm`
 set AS=%BUILD_PREFIX%\Library\bin\nasm.exe
 
-bash ./configure --enable-pic --enable-shared --prefix=%LIBRARY_PREFIX%
+bash ./configure ^
+  --enable-pic ^
+  --enable-shared ^
+  --disable-static ^
+  --prefix=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
 make -j%CPU_COUNT%
