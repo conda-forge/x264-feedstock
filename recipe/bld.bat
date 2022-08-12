cf0e653fb65822cd7352d0b146f1e7ef1b0d0d72
Mark Harfouche <mark.harfouche@gmail.com>
2022-08-12 11:39:07 -0400

    Fixup library names for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,9 +3,12 @@
 rem Set the assembler to `nasm`
 set AS=%BUILD_PREFIX%\Library\bin\nasm.exe
 
+:REM --system-libx264 makes windows choose the shared library for the x264 cli argument
+:REM instead of the static library
 bash ./configure ^
   --enable-pic ^
   --enable-shared ^
+  --system-libx264 ^
   --prefix=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
@@ -13,4 +16,7 @@ make -j%CPU_COUNT%
 if errorlevel 1 exit 1
 
 make install
+if errorlevel 1 exit 1
+
+move %LIBRARY_LIB%\libx264.dll.lib %LIBRARY_LIB%\libx264.lib 
 if errorlevel 1 exit 1
