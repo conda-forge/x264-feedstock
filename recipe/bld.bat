fcc38a339f98b35b70821a43477f555d6d31bdaf
Mark Harfouche <mark.harfouche@gmail.com>
2022-08-12 10:33:23 -0400

    Remove copies of the lib file to make the package smaller

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,7 +15,3 @@ if errorlevel 1 exit 1
 
 make install
 if errorlevel 1 exit 1
-
-:: Make copies of the .lib files without the 'lib' prefix.
-copy %LIBRARY_LIB%\libx264.lib %LIBRARY_LIB%\x264.lib
-if errorlevel 1 exit 1
